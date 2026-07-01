fc29da1c561c5c49149528212e8e912e2eb9dd13
junchi.zhang <435900020@qq.com>
2026-07-01 19:12:58 +0800

    fix: add polyfill for ReadableStream, WritableStream, and TransformStream in webcrypto setup

--- test/setup/webcrypto.cjs
+++ test/setup/webcrypto.cjs
@@ -11,3 +11,19 @@ if (typeof globalThis.crypto?.getRandomValues !== 'function') {
 if (typeof nodeCrypto.getRandomValues !== 'function') {
   nodeCrypto.getRandomValues = webcrypto.getRandomValues.bind(webcrypto);
 }
+
+if (typeof globalThis.ReadableStream !== 'function') {
+  const webStreams = require('stream/web');
+
+  for (const name of ['ReadableStream', 'WritableStream', 'TransformStream']) {
+    if (
+      typeof globalThis[name] !== 'function' &&
+      typeof webStreams[name] === 'function'
+    ) {
+      Object.defineProperty(globalThis, name, {
+        configurable: true,
+        value: webStreams[name],
+      });
+    }
+  }
+}
